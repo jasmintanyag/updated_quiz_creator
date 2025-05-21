6b29e4dc975e121390726d3b73d0983cf5e979fc
jasmintanyag <tanyagjasmin@gmail.com>
2025-05-21 19:22:42 +0800

    Added some changes and fixed the indentions

--- quiz_main.py
+++ quiz_main.py
@@ -9,20 +9,20 @@ class MainMenu:
         self.creator = QuizCreator()
         self.game = QuizGame()
 
-def main_menu():
-    while True:
-        print(f"\n{Fore.YELLOW}----- Quizzierett -----")
-        print("1. Play Quiz")
-        print("2. Exit")
-        choice = input(f"{Fore.MAGENTA}Select an option: ")
-        if choice == "1":
-            play_quiz()
-        elif choice == "2":
-            print(f"{Fore.CYAN}Exiting..... Goodbye!👋")
-            break
-        else:
-            print(f"{Fore.RED}❌ Invalid input!")
-
+    def main_menu(self):
+        while True:
+            print(f"\n{Fore.YELLOW}----- Quizzierett -----")
+            print("1. Play Quiz")
+            print("2. Exit")
+            choice = input(f"{Fore.MAGENTA}Select an option: ")
+            if choice == "1":
+                self.play_quiz()
+            elif choice == "2":
+                print(f"{Fore.CYAN}Exiting..... Goodbye!👋")
+                break
+            else:
+                print(f"{Fore.RED}❌ Invalid input!")
 
 if __name__ == "__main__":
-    main_menu()
+    menu = MainMenu()
+    menu.main_menu()
